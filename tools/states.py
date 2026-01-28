94bb744e1069ce252ee74b00d6d857b50cf551e9
if you dont do it <dontlistenthem@protonmail.com>
2026-01-28 21:01:49 +0300

    Update states.py

--- tools/states.py
+++ tools/states.py
@@ -2,5 +2,6 @@ from aiogram.fsm.state import State, StatesGroup
 
 class Wallets(StatesGroup):
     edit_wallet = State()
+    list_wallet = State()
     track_wallet = State()
     untrack_wallet = State()
