2f3bae66d5541b10d9909c7585d00b591806457a
if you dont do it <dontlistenthem@protonmail.com>
2026-02-05 21:24:41 +0300

    Update main_router.py

--- routers/main_router.py
+++ routers/main_router.py
@@ -2,10 +2,14 @@ from aiogram import Router
 
 from .chat_routers import chat_router
 from .callbacks import callback_router
+from .primary_routers import primary_router
+from .fsm import blockchain_router
 
 main_router = Router()
 
 main_router.include_routers(
+    primary_router,
     chat_router,
-    callback_router
+    callback_router,
+    blockchain_router
 )
