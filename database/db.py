413b8e36b819c577c194168a95895312f5787ad8
if you dont do it <dontlistenthem@protonmail.com>
2026-02-09 22:52:00 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -6,54 +6,61 @@ async def sql3_table() -> None:
             csr = await cnn.cursor()
             
             await csr.execute('CREATE TABLE IF NOT EXISTS Clients ('
-                            'client_id BIGINT, '
-                            'row TEXT)'
-                            )
+                              'client_id BIGINT, '
+                              'row TEXT)'
+                              )
         finally:
             if csr != None:
                 await csr.close()
             
     
 async def sql3_remove_row(client, row) -> None:
-    async with aiosqlite.connect('Addresses.sqlite', isolation_level=None) as cnn:
+    async with aiosqlite.connect('Addresses.sqlite') as cnn:
         try:
             csr = await cnn.cursor()
             
-            await csr.execute('DELETE FROM Multichain Clients '
-                            'WHERE client_id = ? '
-                            'AND row = ?', 
-                            (client, row)
-                            )
+            data = await csr.execute('DELETE FROM Clients '
+                              'WHERE client_id = ? ' 
+                              'AND row = ?'
+                              (client, row)
+                              )
+            if csr.rowcount > 0:
+                return data
         finally:
             if csr != None:
                 await csr.close()
-            
+                
 async def sql3_receipt_row(client) -> None:
-    async with aiosqlite.connect('Addresses.sqlite', isolation_level=None) as cnn:
+    async with aiosqlite.connect('Addresses.sqlite') as cnn:
         try:
             csr = await cnn.cursor()
             
-            await csr.execute('SELECT * FROM Clients '
-                                'WHERE client_id = ?',
-                                (client, )
-                                )
+            await csr.execute('SELECT row FROM Clients '
+                              'WHERE client_id = ? '
+                              'LIMIT 5',
+                              (client, )
+                              )
             
-            data = await csr.fetchall()
+            raw_data = await csr.fetchall()
+            
+            data = []
+            for row in raw_data:
+                data.append(row)
             return data
         finally:
             if csr != None:
                 await csr.close()
-        
+
 async def sql3_addopt_row(client, row) -> None:
     async with aiosqlite.connect('Addresses.sqlite', isolation_level=None) as cnn:
         try:
             csr = await cnn.cursor()
             
             await csr.execute('INSERT INTO Clients '
-                            '(client_id, row) '
-                            'VALUES (?, ?)',
-                            (client, row)
-                            )
+                              '(client_id, row) '
+                              'VALUES (?, ?)',
+                              (client, row)
+                              )
         finally:
             if csr != None:
                 await csr.close()
