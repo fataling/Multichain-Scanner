2ff5800234c60126fa9adc3d72faf786d38ee71d
if you dont do it <dontlistenthem@protonmail.com>
2026-02-19 22:22:22 +0300

    Create alarm_buttons.py

---
+++ alarms/alarm_buttons.py
@@ -0,0 +1,20 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+def etherscan_button(url):
+    return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text='EtherScan',
+                                        url=url)]])
+     
+def bnbscan_button(url):
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='BscScan',
+                                    url=url)]])
+
+def tonviewer_button(url):
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='Tonviewer Exployer',
+                                    url=url)]
+                    ])
