9640792acccb796a8f2428963e593a002863c6b0
if you dont do it <dontlistenthem@protonmail.com>
2026-03-10 05:46:57 +0300

    Update cfg.py

--- tools/cfg.py
+++ tools/cfg.py
@@ -1,3 +1,10 @@
+from tokens import token_bot
+from aiogram import Bot
+
 import logging
-logging.basicConfig(level=logging.INFO, format="%(message)s")
-log = logging.info
+
+logging.basicConfig(level=logging.INFO, 
+                    format="%(message)s")
+log = logging
+
+bot = Bot(token=token_bot)
