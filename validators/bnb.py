401a82b81d983b9538c1e54b4dbeb5934a6c9db5
if you dont do it <dontlistenthem@protonmail.com>
2026-03-13 19:58:10 +0300

    Create bnb.py

---
+++ validators/bnb.py
@@ -0,0 +1,22 @@
+from eth_utils import (
+    is_checksum_address,
+    is_address,
+    is_hex_address
+)
+
+from typing import Optional
+
+def valid_string_bnb(data: str) -> Optional[bool]:
+    if not isinstance(data, str):
+        return False
+    
+    if not is_hex_address(data):
+        return False
+    
+    if not is_address(data):
+        return False
+
+    if not is_checksum_address(data):
+        return False
+    
+    return True
