04af1900c98cc451a3fd496afbe6b6c481a61975
if you dont do it <dontlistenthem@protonmail.com>
2026-03-13 19:59:00 +0300

    Update ton.py

--- validators/ton.py
+++ validators/ton.py
@@ -1,12 +1,13 @@
 from pytoniq_core import Address, AddressError
 
-def valid_string_ton(data: str):
+from typing import Optional
+
+def valid_string_ton(data: str) -> Optional[bool]:
     if not isinstance(data, str):
-        raise ValueError(
-            f'this type is incorrect'
-        )
-    
-    if not Address(data):
-        raise AddressError(
-            f'This is invalid'
-        )
+        return False
+    try:
+        if not Address(data):
+            raise AddressError
+    except:
+        return False
+    return True
