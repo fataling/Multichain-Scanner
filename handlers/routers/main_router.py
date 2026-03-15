ca311dbed287862439a17c3752f31900c6ee040c
if you dont do it <dontlistenthem@protonmail.com>
2026-03-15 12:28:12 +0300

    Update main_router.py

--- handlers/routers/main_router.py
+++ handlers/routers/main_router.py
@@ -3,16 +3,16 @@ from aiogram import Router
 from .start_router import start_router
 from ..callbacks.data_seed import seed_router
 
-from ..callbacks.data_choice_chain import choice_chain_router
-from .reg_router import reg_router
+from ..callbacks.data_choice_chain import chain_router
 from ..callbacks.data_remove import remove_router
+from .reg_router import reg_router
 
 main_router = Router()
 
 main_router.include_routers(
     start_router,
     seed_router,
-    choice_chain_router,
+    chain_router,
     reg_router,
     remove_router
 )
