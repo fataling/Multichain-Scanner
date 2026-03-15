b78cbc430cb2c5de5f1d743fbdf647df5102bcca
if you dont do it <dontlistenthem@protonmail.com>
2026-03-15 12:26:55 +0300

    Update keyboard.py

--- keyboards/keyboard.py
+++ keyboards/keyboard.py
@@ -6,44 +6,44 @@ action_keyboard = InlineKeyboardMarkup(
                                         callback_data='track'), 
                         InlineKeyboardButton(text='📒 Wallets',
                                         callback_data='my_wallets')]], 
-                                                input_field_placeholder='Type me Satoshi address...')
+                                                input_field_placeholder='What are we going to do today?')
 
 blockchain_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
                 [InlineKeyboardButton(text='Ethereum',
-                                      callback_data='eth_chain'),
-                 InlineKeyboardButton(text='Binance Coin',
-                                     callback_data='bnb_chain')],
-                [InlineKeyboardButton(text='Solana',
-                               callback_data='sol_chain'),
+                                      callback_data='eth_chain')],
+                [InlineKeyboardButton(text='Binance Coin',
+                                     callback_data='bnb_chain'),
                  InlineKeyboardButton(text='Toncoin',
                                      callback_data='ton_chain')],
+                [InlineKeyboardButton(text='Solana',
+                               callback_data='sol_chain')],
                 [InlineKeyboardButton(text='‹ Back',
                                       callback_data='back_menu')]
-                        ],
-                                        input_field_placeholder='Tell me his...')
+                        ], input_field_placeholder='Choose your chain...')
 
-
-wallets_keyboard = InlineKeyboardMarkup(
+data_action_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
                 [InlineKeyboardButton(text='📌 Add',
                                       callback_data='track'),
                 InlineKeyboardButton(text='❌ Delete',
-                                     callback_data='untrack')],
+                                     callback_data='delete_rows')],
                 [InlineKeyboardButton(text='⭕ Delete all',
-                                      callback_data='delete_all')],
+                                      callback_data='delete_all_rows')],
                 [InlineKeyboardButton(text='‹ Back',
-                                      callback_data='back_menu')]
-                        ]
-        )
+                                      callback_data='back_menu')],
+                        ], input_field_placeholder='What do you want to do?')
 
-confirm_keyboard = InlineKeyboardMarkup(
+confirm_action_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
                 [InlineKeyboardButton(text='Yes',
-                                      callback_data='pidor'),
-                 InlineKeyboardButton(text='No',
-                                     callback_data='popa')],
+                                      callback_data='yes_all'),
+                InlineKeyboardButton(text='No',
+                                     callback_data='no_all')],
+                        ], input_field_placeholder='Confirm your action!')
+
+back_menu_keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
                 [InlineKeyboardButton(text='‹ Back',
-                               callback_data='back_menu')],
-                        ],
-                                        input_field_placeholder='Tell me his...')
+                                      callback_data='back_menu')]
+                        ], input_field_placeholder='Not a step forward!')
