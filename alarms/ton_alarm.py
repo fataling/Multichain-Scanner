c7ad922f5aa12c5fd9dca5cdcf9bb60bf24b3e2e
if you dont do it <dontlistenthem@protonmail.com>
2026-03-15 12:16:57 +0300

    Create ton_alarm.py

---
+++ alarms/ton_alarm.py
@@ -0,0 +1,46 @@
+from others.cfg import bot, log
+from keyboards.alarm_buttons import tonviewer_button
+
+from aiogram.exceptions import (
+    TelegramNetworkError,
+    TelegramUnauthorizedError,
+    TelegramForbiddenError
+    )
+from aiogram.enums import ParseMode
+from aiogram.types import LinkPreviewOptions
+
+async def ton_data_alarm(data_for_alarm: dict) -> None:
+    ton_row = data_for_alarm['row']
+    hash_tx = data_for_alarm['hash']  
+       
+    url = f'https://tonviewer.com/transaction/{hash_tx}'
+    url_acc = f'https://tonviewer.com/{ton_row}'
+    
+    data_for_alarm['url'] = url
+    data_for_alarm['url_acc'] = url_acc
+    
+    short_url = f"{ton_row[:6]}...{ton_row[-4:]}"
+    data_for_alarm['short_url'] = short_url
+    
+    await alarm_to_chat(data_for_alarm)
+
+async def alarm_to_chat(data_for_alarm: dict) -> None:
+    button = tonviewer_button(data_for_alarm['url'])
+    
+    try:
+        await bot.send_message(chat_id=data_for_alarm['chat_id'],
+                               text=f'🔔 <b>Transaction is found!</b>\n\n'
+                                     f"👛 <b>Wallet:</b> <a href='{data_for_alarm['url_acc']}'>{data_for_alarm['short_url']}</a>\n"
+                                     f"📬 Type: <b>{data_for_alarm['type_tx']}</b>\n"
+                                     f"💎 Value: <code>{data_for_alarm['amount_tx']}</code> TON",
+                               reply_markup=button,
+                               link_preview_options=LinkPreviewOptions(
+                                   is_disabled=True
+                                   ),
+                               parse_mode=ParseMode.HTML)
+    except TelegramNetworkError as a:
+        log(f'A network error occurred, please try again later! - {a}')
+    except TelegramUnauthorizedError as b:
+        log(f'Maybe, bot not detected! - {b}')
+    except TelegramForbiddenError as c:
+        log(f'Bot not have access to have chat! - {c}')
